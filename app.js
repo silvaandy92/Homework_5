bbcd7775f52498268f5fdc40249999382933a180
Your Name <you@example.com>
2020-05-01 17:53:54 -0700

    2nd Commit

--- app.js
+++ app.js
@@ -1,31 +1,57 @@
 function findPrime ()   {
-    "use strict";
     
     var i, j, c, start, end;
-    // get the first and the second value from the form
 
-    start = parseInt(document.getElementById('start').value);
-    end = parseInt(document.getElementById('end').value);
+    start = parseInt(document.getElementById('number1').value);
+    end = parseInt(document.getElementById('number2').value);
 
-    //clear the result div
     document.getElementById('result').innerHTML = '';
 
-    //loop til i equals to end
     for (i = start; i <= end; i++) {
         c = 0;
         for (j = 1; j <= i; j++) {
-            // % modules will give the reminder value, so if the reminder is 0 then it is divisible
+
             if (i % j === 0) {
-                //increment the value of c
+
                 c++;
             }
         }
 
-        //if the value of count is 2 then is is a prime number
-        //because a prime number should be exactly divisible by 2 times only (itself and 1)
         if (c === 2) {
             document.getElementById('statement').innerHTML = `There are ${c} prime numbers.`;
             document.getElementById('result').insertAdjacentHTML('beforeend', i + ',')
         }
     }
 }
+
+document.getElementById('number1').addEventListener('blur', e => {
+    const num1 = e.target.value;
+    let num1output = '';
+    let messageColor='red';
+    if (num1 > 100) {
+        num1output = 'Invalid input, please try again.'
+        messageColor = 'red';
+    } else if ( num1 < 2) {
+        num1output = 'Invalid input, please try again.';
+        messageColor = 'red';
+    }
+    const number1HelpElement = document.getElementById('number1Help');
+    number1HelpElement.textContent = `${num1output}`;
+    number1HelpElement.style.color = messageColor;
+})
+
+document.getElementById('number2').addEventListener('blur', e => {
+    const num2 = e.target.value;
+    let num2output = '';
+    let messageColor='red';
+    if (num2 > 100) {
+        num2output = 'Invalid input, please try again.'
+        messageColor = 'red';
+    } else if ( num2 < 2) {
+        num2put = 'Invalid input, please try again.';
+        messageColor = 'red';
+    }
+    const number1HelpElement = document.getElementById('number1Help');
+    number1HelpElement.textContent = `${num2output}`;
+    number1HelpElement.style.color = messageColor;
+})
